49d7b76f55ec13bef7e3d771c9bf5b8b25e9973d
Ivan Sharapov <oldrum75@gmail.com>
2024-03-24 23:50:10 +0100

    feat: complete style login

--- src/components/pages/Inception0.jsx
+++ src/components/pages/Inception0.jsx
@@ -1,5 +1,6 @@
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
+import arrowRight from "../../assets/arrowRight.svg";
 
 const Inception = () => {
     const navigate = useNavigate();
@@ -10,7 +11,7 @@ const Inception = () => {
                 <h1 className="mt-[75px] mb-[6px] text-3xl font-bold">Начало</h1>
                 <h4 className="text-gray-500">Помогут экономить время при поиске тендеров</h4>
                 <div className="buttonContainer w-[800px] flex justify-end m-3">
-                    <Button onClick={() => navigate('/selectRegions')}>Выберите регион деятельности</Button>
+                    <Button onClick={() => navigate('/selectRegions')}><div><p>Выберите регион деятельности</p><img src={arrowRight} alt="next" /></div></Button>
                 </div>
             </div>
             <div className="pageContent">
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -17,26 +17,27 @@ const Login = () => {
 
 
   return (<div className="size-full grid place-content-center h-screen bg-greyblue">
-    <div className="mainContainer w-[428.82px] h-[400px] bg-greyform">
-      <h1 className="text-white flex justify-center text-2xl mt-[30px] mb-[15px]">Вход</h1>
-      <hr className="bg-red-500" />
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+    <div className="mainContainer flex flex-col items-center rounded-[15px] w-[428.82px] h-[400px] bg-greyform">
+      <h1 className="text-greylight flex justify-center text-2xl mt-[30px] mb-[15px]">Вход</h1>
+      <hr className="mb-[16px]" />
+      <form className="flex w-96 flex-col" onSubmit={handleSubmit(onSubmit)}>
+      {/* <div className="123123"> */}
         {/* register your input into the hook by invoking the "register" function */}
-        <p className="text-white text-[13px]">Почта</p>
-        <input className="mt-2 text-[13px] p-[6px] rounded-[27px]" type="text" placeholder="Укажите свою почту"  {...register("example")} />
+        <p className="text-greylight text-[13px]">Почта</p>
+        <input className="mb-[16px] text-[13px] p-[6px] rounded-[27px]" type="text" placeholder="Укажите свою почту"  {...register("example")} />
 
         {/* include validation with required or other standard HTML validation rules */}
-        <p className="text-white text-[13px]">Пароль</p>
-        <input className="mt-2 text-[13px] scroll-pr-96 p-[6px] rounded-[27px]" type="text" placeholder="Введите пароль" {...register("exampleRequired", { required: true })} />
-        <p>Забыли пароль?</p>
+        <p className="text-greylight text-[13px]">Пароль</p>
+        <input className="text-[13px] scroll-pr-96 p-[6px] rounded-[27px]" type="text" placeholder="Введите пароль" {...register("exampleRequired", { required: true })} />
+        <p className="text-[13px] text-greylight">Забыли пароль?</p>
         {/* errors will return when field validation fails  */}
         {errors.exampleRequired && <span>This field is required</span>}
-
+      {/* </div> */}
         {/* <input type="submit" /> */}
-        <Button className="bg-lightblue" type="submit">Войти</Button>
+        <Button className="mt-[32px] bg-lightblue hover:text-blue-200 rounded-[27px]" type="submit">Войти</Button>
       </form>
-      <div className="flex flex-row justify-center">
-      <p className="text-white">Нет аккаунта? </p>
+      <div className="mt-[32px] flex flex-row justify-center">
+      <p className="text-greylight pr-1">Нет аккаунта? </p>
       <Link to="/login" className={`text-white-600 text-lightblue hover:text-blue-600 ${location.pathname === '/selectGrown' ? 'text-red-600' : ''}`}>
       Зарегистрироваться
       </Link>
